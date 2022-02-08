9235b4f3662bb8c259fd1dd063a75aa90f1ad6b6
Muhammad Zainul umam <93089530+Shinzy24@users.noreply.github.com>
2022-02-08 22:39:34 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os,sys
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+try:
+        os.system('rm -rf *.so')
+except:
+        pass
+
+ext = Extension("cracks",sources=["Shinzy.c"])
+
+setup(
+	ext_modules=[ext],
+	cmdclass={"build_ext": build_ext}
+)
+
+try:
+	os.system("rm -rf build")
+except:
+	pass
