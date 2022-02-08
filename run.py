7c205f94176111124f0c9c88dcff5b2b29f129f9
Muhammad Zainul umam <93089530+Shinzy24@users.noreply.github.com>
2022-02-08 22:36:25 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,21 @@
-if __name__ == "__main__":
-	try:
-		__import__("Shinzy").make()
-	except Exception as e:
-		exit(str(e))
+import os,sys
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+try:
+        os.system('rm -rf *.so')
+except:
+        pass
+
+ext = Extension("cracks",sources=["Shinzy.c"])
+
+setup(
+	ext_modules=[ext],
+	cmdclass={"build_ext": build_ext}
+)
+
+try:
+	os.system("rm -rf build")
+except:
+	pass
